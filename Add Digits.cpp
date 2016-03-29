cf64fc05de5f5d4d4e5ab55e17b73fff06f86885
5277 <bwaiqqs@163.com>
2016-03-29 21:11:08 +0800

    Create Add Digits.cpp

---
+++ Add Digits.cpp
@@ -0,0 +1,11 @@
+public class Solution {
+    public int addDigits(int num) {
+        if (num == 0)
+            return 0;
+        int a = num % 9;
+        if(a == 0)
+            return 9;
+        else
+            return a;
+    }
+}
